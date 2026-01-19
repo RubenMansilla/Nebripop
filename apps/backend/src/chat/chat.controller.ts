0b9cb15b1aee858b90ecd87b1d9314d3a86cb662
Kiwan33 <rubenmansillar06@gmail.com>
2026-01-20 00:35:07 +0100

    Update log message in chat controller
    Modified the console log in ChatController to clarify the event emission to the chat room, adding '(porfi)' for additional context.

--- apps/backend/src/chat/chat.controller.ts
+++ apps/backend/src/chat/chat.controller.ts
@@ -59,7 +59,7 @@ export class ChatController {
 
         // Definir el nombre de la sala 
         const roomName = `chat_${chatId}`;
-        console.log(`🔔 HTTP -> SOCKET: Emitiendo evento 'new_message' a sala '${roomName}'`);
+        console.log(`🔔 HTTP -> SOCKET: Emitiendo evento 'new_message' a sala (porfi) '${roomName}'`);
 
         // Emitir el evento a esa sala
         // Enviar el objeto extendido con chatId para facilitar la vida al frontend
